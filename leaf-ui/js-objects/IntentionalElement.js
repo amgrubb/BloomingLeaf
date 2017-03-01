83e01e42566f1417c6b9b28f12be6f1e19164c8d
A.M.Grubb <amgrubb@cs.toronto.edu>
2017-03-01 16:22:15 -0500

    Added .js files for each element.

---
+++ leaf-ui/js-objects/IntentionalElement.js
@@ -0,0 +1,13 @@
+var IntentionalElement = new function(){
+	this.name = "NAME";
+	this.id = "NO-ID";
+	this.linksSrc = []; //ElementLink
+	this.linksDest = []; //ElementLink
+	this.iStarSpec;
+	this.actor;
+	this.type;
+	this.dynamicType;
+	this.decompositionType;
+}
+
+	
