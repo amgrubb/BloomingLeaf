2f6e0f00b11dca774c01a8d98c32e615c2a3a113
Marcel Akira <marcel.serikawa@gmail.com>
2017-03-01 15:09:56 -0500

    Adding js classes

---
+++ leaf-ui/js/IStarSpec.js
@@ -0,0 +1,20 @@
+var IStarSpec = function(){
+	this.intElements = []; //IntentionalElement
+	this.actors = []; //Actor
+	this.evolutionLink = []; //EvolutionLink 
+	this.contribution = []; //Contribution
+	this.decomposition = []; //Decomposition
+	this.dependency = []; //Dependency
+	this.precondition = []; //Precondition
+	this.strategyCollection = []; //EvaluationStrategy;
+	this.constraintsBetweenEpochs = []; //EpochConstraint
+	this.simulationType = 0;
+	this.maxTime = 5;
+	this.maxEpoch = 5;
+	this.history; //int[][][] history;
+	this.relativeTimePoints = 0;
+	this.absoluteTimePoints = [];
+}
+
+
+ 	
