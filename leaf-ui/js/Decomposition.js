7977c002ed9ce69856a391b6a5974826fe5623d6
Marcel Akira <marcel.serikawa@gmail.com>
2017-03-01 16:23:33 -0500

    Working on the js objects

---
+++ leaf-ui/js/Decomposition.js
@@ -0,0 +1,7 @@
+function Decomposition(src, dest, type = "link", activeLink = true, decomposition = "AND"){
+	this.src = src;
+	this.dest = dest;
+	this.type = type;
+	this.activelink = activeLink;
+	this.decomposition = decomposition;	
+}
--- leaf-ui/js/IStarSpec.js
+++ leaf-ui/js/IStarSpec.js
@@ -14,24 +14,6 @@ function IStarSpec(intElements, actors, evolutionLink, contribution, decompositi
 	this.history; //int[][][] history;
 	this.relativeTimePoints = 0;
 	this.absoluteTimePoints = []//Array;
-	
-	
-//	this.intElements = []; //IntentionalElement
-//	this.actors = []; //Actor
-//	this.evolutionLink = []; //EvolutionLink 
-//	this.contribution = []; //Contribution
-//	this.decomposition = []; //Decomposition
-//	this.dependency = []; //Dependency
-//	this.precondition = []; //Precondition
-//	this.strategyCollection = []; //EvaluationStrategy;
-//	this.constraintsBetweenEpochs = []; //EpochConstraint
-//	this.simulationType = 0;
-//	this.maxTime = 5;
-//	this.maxEpoch = 5;
-//	this.history; //int[][][] history;
-//	this.relativeTimePoints = 0;
-//	this.absoluteTimePoints = [];
 }
 
-
- 	
+	
