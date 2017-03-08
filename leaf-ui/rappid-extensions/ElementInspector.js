7fb0601bc012f921b1cf4dd2de6711704e2998ae
Navie <naviechan@gmail.com>
2017-03-08 17:50:15 -0500

    Updated the dropdown of sat value in element inspector

--- leaf-ui/rappid-extensions/ElementInspector.js
+++ leaf-ui/rappid-extensions/ElementInspector.js
@@ -29,13 +29,12 @@ var ElementInspector = Backbone.View.extend({
       '<textarea class="cell-attrs-text"></textarea>',
       '<label>Initial Satisfaction Value</label>',
       '<select id="init-sat-value">',
-        '<option value=none> None </option>',
-        '<option value=conflict> Conflict </option>',
-        '<option value=satisfied> Satisfied </option>',
-        '<option value=partiallysatisfied> Partially Satisfied </option>',
+        '<option value=none> None (T, T)</option>',
+        '<option value=satisfied> Satisfied (FS, T)</option>',
+        '<option value=partiallysatisfied> Partially Satisfied (PS, T) </option>',
+        '<option value=denied> Denied (T, FD)</option>',
+        '<option value=partiallydenied> Partially Denied (T, FD)</option>',
         '<option value=unknown> Unknown </option>',
-        '<option value=partiallydenied> Partially Denied </option>',
-        '<option value=denied> Denied </option>',
 			'</select>',
       '<br>',
       '<div id="function-div">',
@@ -711,19 +710,16 @@ var ElementInspector = Backbone.View.extend({
     // Navie: Changed satvalue from path to text
     if (value == "satisfied"){
       // cell.attr({ '.satvalue': {'d': 'M 0 10 L 5 20 L 20 0 L 5 20 L 0 10', 'stroke': '#00FF00', 'stroke-width':4}});      
-      cell.attr(".satvalue/text", "S");
+      cell.attr(".satvalue/text", "(FS, T)");
     }else if(value == "partiallysatisfied") {
       // cell.attr({ '.satvalue': {'d': 'M 0 8 L 5 18 L 20 0 L 5 18 L 0 8 M 17 30 L 17 15 C 17 15 30 17 18 23', 'stroke': '#00FF00', 'stroke-width':3, 'fill': 'transparent'}});
-      cell.attr(".satvalue/text", "PS");
+      cell.attr(".satvalue/text", "(PS, T)");
     }else if (value == "denied"){
       // cell.attr({ '.satvalue': {'d': 'M 0 20 L 20 0 M 10 10 L 0 0 L 20 20', 'stroke': '#FF0000', 'stroke-width': 4}});
-      cell.attr(".satvalue/text", "D");
+      cell.attr(".satvalue/text", "(T, FD)");
     }else if (value == "partiallydenied") {
       // cell.attr({ '.satvalue': {'d': 'M 0 15 L 15 0 M 15 15 L 0 0 M 17 30 L 17 15 C 17 15 30 17 18 23', 'stroke': '#FF0000', 'stroke-width': 3, 'fill': 'transparent'}});
-      cell.attr(".satvalue/text", "PD");
-    }else if (value == "conflict") {
-      // cell.attr({ '.satvalue': {'d': 'M 0 0 L 20 8 M 20 7 L 5 15 M 5 14 L 25 23', 'stroke': '#222222', 'stroke-width': 4}});
-      cell.attr(".satvalue/text", "?");
+      cell.attr(".satvalue/text", "(T, PD)");
     }else if (value == "unknown") {
       // cell.attr({ '.satvalue': {'d': 'M15.255,0c5.424,0,10.764,2.498,10.764,8.473c0,5.51-6.314,7.629-7.67,9.62c-1.018,1.481-0.678,3.562-3.475,3.562\
           // c-1.822,0-2.712-1.482-2.712-2.838c0-5.046,7.414-6.188,7.414-10.343c0-2.287-1.522-3.643-4.066-3.643\
