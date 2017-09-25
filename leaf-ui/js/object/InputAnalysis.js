6f83e7d575713203b605fb8a8ad35b42419ae885
A. M. Grubb <amgrubb@cs.toronto.edu>
2017-09-25 15:44:47 -0400

    Hot Fix Oops
    - One more quick fix.

--- leaf-ui/js/object/InputAnalysis.js
+++ leaf-ui/js/object/InputAnalysis.js
@@ -5,32 +5,32 @@ function InputAnalysis(){
 	this.numRelTime = null;
 	this.absTimePts = null;
 	this.currentState = "0";
-	this.initialAssignedEpoch = "0";
-	this.initialValueTimePoints = "0";
+	this.initialAssignedEpoch = ["0"];
+	this.initialValueTimePoints = ["0"];
 	this.elementList = null;
 }
 
 function getAnalysisValues(analysisInterface){
-	//Data required for 1. Simulate Single Path 
+	//Data required for 1. Simulate Single Path
 	analysisInterface.maxAbsTime = $('#max-abs-time').val();
 	analysisInterface.conflictLevel = $('#conflict-level').val();
 	analysisInterface.numRelTime = $('#num-rel-time').val();
 	if($('#abs-time-pts').val()){
-		analysisInterface.absTimePts = $('#abs-time-pts').val();		
+		analysisInterface.absTimePts = $('#abs-time-pts').val();
 	}
 	analysisInterface.elementList = getElementList();
-	
+
 	//Data required for 2. Explore Possible Next States
 	if(savedAnalysisData.finalAssigneEpoch){
-		analysisInterface.initialAssignedEpoch = savedAnalysisData.finalAssigneEpoch;		
+		analysisInterface.initialAssignedEpoch = savedAnalysisData.finalAssigneEpoch;
 	}
-	
+
 	if(savedAnalysisData.finalValueTimePoints){
-		analysisInterface.initialValueTimePoints = savedAnalysisData.finalValueTimePoints;		
+		analysisInterface.initialValueTimePoints = savedAnalysisData.finalValueTimePoints;
 	}
-	
+
 	if($('#sliderValue').text()){
-		analysisInterface.currentState = $('#sliderValue').text();		
+		analysisInterface.currentState = $('#sliderValue').text();
 	}
 	analysisInterface.numRelTime = $('#num-rel-time').val();
 
@@ -48,7 +48,7 @@ function getElementList(){
 			"denied": "1100",
 			"none": "0000"
 		};
-	
+
 	if(!global_analysisResult.elementList){
 		for(var i = 0; i < this.graph.getElements().length; i++){
 			var element= {};
@@ -57,23 +57,23 @@ function getElementList(){
 			//Making currentValue to numeric values like 0000, 0001, 0011...
 			if(!$.isNumeric(currentValue))
 				currentValue = satValueDict[currentValue];
-			
+
 			//Making that the elementId has 4 digits
 			var elementID = intentionsCount.toString();
-			while (elementID.length < 4){ 
+			while (elementID.length < 4){
 				elementID = "0" + elementID;
 				}
 			//Adding the new id to the UI graph element
 			this.graph.getElements()[i].prop("elementid", elementID);
-			
+
 			element.id = elementID;
 			element.status = [];
 			element.status.push(currentValue);
-			
+
 			intentionsCount++;
 			elementList.push(element);
 		}
-		return elementList;		
+		return elementList;
 	}else{
 		var element = {};
 		var index = $('#sliderValue').text().split('|');
@@ -81,9 +81,9 @@ function getElementList(){
 		global_analysisResult.elementList.forEach(function(currentEl){
 				element.id = currentEl.id;
 				element.status = currentEl.status.splice(0, i_splice);
-				elementList.push(element);	
+				elementList.push(element);
 			}
 		);
-		return elementList;		
+		return elementList;
 	}
 }
