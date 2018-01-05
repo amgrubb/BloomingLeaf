ff449cae72d59a5189914e1a4fa281f4cf10c38d
Hanbin Chang <hanbin.chang@mail.utoronto.ca>
2018-01-05 01:43:49 -0500

    More test cases

--- leaf-ui/blooming_test/testScript.js
+++ leaf-ui/blooming_test/testScript.js
@@ -109,9 +109,193 @@ var source4 = (function() {
   };
 })();
 
+// syntax run for all or links
+var source_syntax1 =  (function() {
+  var sourceData;
+  var sourceData2;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck1.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+// syntax run for and links and one or links
+var source_syntax2 =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck2.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+// syntax run for all or links
+var source_syntax3 =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck3.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+// syntax run for or links and one and links
+var source_syntax4 =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck4.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+var source_syntax5 =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck5.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+var source_syntax6a =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck6a.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+var source_syntax6b =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck6b.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+var source_syntax7 =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck7.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+var source_syntax8a =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck8a.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
+
+var source_syntax8b =  (function() {
+  var sourceData;
+  function getData(done) {
+    if(sourceData){
+      done(sourceData);
+    } else {
+      $.getJSON("./blooming_test/syntaxCheck8b.json", function(data) {
+        sourceData = data
+        console.log(sourceData);
+        done(data);
+      });
+    }
+  }
+  return {
+    getData: getData
+  };
+})();
 
 describe('Cycle Check 1', function() {
-  it('Loop 1 with single graph', function () {
+  it('Loop 1 with single graph', function (done) {
      source.getData(function(sourceData){
        var analysis = new InputAnalysis();
      	 var links = new InputLink();
@@ -127,9 +311,10 @@ describe('Cycle Check 1', function() {
        var testNodes = getElementList();
        var isCycle = cycleCheck(testLink,testNodes);
        expect(isCycle).to.equal(true);
+       done();
      })
   })
-  it('Loop 2 with multiple graphs', function () {
+  it('Loop 2 with multiple graphs', function (done) {
      source4.getData(function(sourceData){
        var analysis = new InputAnalysis();
      	 var links = new InputLink();
@@ -145,9 +330,10 @@ describe('Cycle Check 1', function() {
        var testNodes = getElementList();
        var isCycle = cycleCheck(testLink,testNodes);
        expect(isCycle).to.equal(true);
+       done();
      })
   })
-  it('Loop 3 with same number of nodes as links', function () {
+  it('Loop 3 with same number of nodes as links', function (done) {
      source4.getData_2(function(sourceData){
        var analysis = new InputAnalysis();
      	 var links = new InputLink();
@@ -163,6 +349,7 @@ describe('Cycle Check 1', function() {
        var testNodes = getElementList();
        var isCycle = cycleCheck(testLink,testNodes);
        expect(isCycle).to.equal(true);
+       done();
      })
   })
 });
@@ -231,7 +418,226 @@ describe('Cycle Check 1', function() {
     });
 })();
 
+(function() {
+  describe('SyntaxCheck for valid combinations', function() {
+    it('Syntax Check with all and constraint links', function (done) {
+       source_syntax1.getData(function(sourceData){
+         var analysis = new InputAnalysis();
+         var links = new InputLink();
+         var js_object = {};
+         var js_links = getLinks();
+         js_object.analysis = getElementList(analysis);
+         graph.fromJSON(sourceData);
+         this.graphList = graph.getElements();
+         this.links = graph.getLinks();
+         console.log(getElementList());
+         console.log(getLinks());
+         var testLink = getLinks();
+         var testNodes = getElementList();
+         var SyntaxCheck1 = returned_syntaxCheck();
+         console.log("does it reach here?")
+         console.log(SyntaxCheck1);
+         expect(SyntaxCheck1).to.equal(false);
+         done();
+       })
+    })
+    it('Syntax Check with all or constraint links', function (done) {
+       source_syntax3.getData(function(sourceData){
+         var analysis = new InputAnalysis();
+         var links = new InputLink();
+         var js_object = {};
+         var js_links = getLinks();
+         js_object.analysis = getElementList(analysis);
+         graph.fromJSON(sourceData);
+         this.graphList = graph.getElements();
+         this.links = graph.getLinks();
+         console.log(getElementList());
+         console.log(getLinks());
+         var testLink = getLinks();
+         var testNodes = getElementList();
+         var SyntaxCheck1 = returned_syntaxCheck();
+         console.log("does it reach here?")
+         console.log(SyntaxCheck1);
+         expect(SyntaxCheck1).to.equal(false);
+         done();
+       })
+    })
+    it('Syntax Check with all ++ constraint links', function (done) {
+       source_syntax5.getData(function(sourceData){
+         var analysis = new InputAnalysis();
+         var links = new InputLink();
+         var js_object = {};
+         var js_links = getLinks();
+         js_object.analysis = getElementList(analysis);
+         graph.fromJSON(sourceData);
+         this.graphList = graph.getElements();
+         this.links = graph.getLinks();
+         console.log(getElementList());
+         console.log(getLinks());
+         var testLink = getLinks();
+         var testNodes = getElementList();
+         var SyntaxCheck1 = returned_syntaxCheck();
+         console.log("does it reach here?")
+         console.log(SyntaxCheck1);
+         expect(SyntaxCheck1).to.equal(false);
+         done();
+       })
+    })
+    it('Syntax Check with all -- constraint links', function (done) {
+       source_syntax7.getData(function(sourceData){
+         var analysis = new InputAnalysis();
+         var links = new InputLink();
+         var js_object = {};
+         var js_links = getLinks();
+         js_object.analysis = getElementList(analysis);
+         graph.fromJSON(sourceData);
+         this.graphList = graph.getElements();
+         this.links = graph.getLinks();
+         console.log(getElementList());
+         console.log(getLinks());
+         var testLink = getLinks();
+         var testNodes = getElementList();
+         var SyntaxCheck1 = returned_syntaxCheck();
+         console.log("does it reach here?")
+         console.log(SyntaxCheck1);
+         expect(SyntaxCheck1).to.equal(false);
+         done();
+       })
+    })
+});
+})();
 
+describe('SyntaxCheck for invalid combinations', function() {
+  it('Syntax Check with and constraint links and a single or link', function (done) {
+     source_syntax2.getData(function(sourceData){
+       var analysis = new InputAnalysis();
+       var links = new InputLink();
+       var js_object = {};
+       var js_links = getLinks();
+       js_object.analysis = getElementList(analysis);
+       graph.fromJSON(sourceData);
+       this.graphList = graph.getElements();
+       this.links = graph.getLinks();
+       console.log(getElementList());
+       console.log(getLinks());
+       var testLink = getLinks();
+       var testNodes = getElementList();
+       var SyntaxCheck1 = returned_syntaxCheck();
+       console.log("does it reach here?")
+       console.log(SyntaxCheck1);
+       expect(SyntaxCheck1).to.equal(true);
+       done();
+     })
+  })
+  it('Syntax Check with or constraint links and a single and link', function (done) {
+     source_syntax4.getData(function(sourceData){
+       var analysis = new InputAnalysis();
+       var links = new InputLink();
+       var js_object = {};
+       var js_links = getLinks();
+       js_object.analysis = getElementList(analysis);
+       graph.fromJSON(sourceData);
+       this.graphList = graph.getElements();
+       this.links = graph.getLinks();
+       console.log(getElementList());
+       console.log(getLinks());
+       var testLink = getLinks();
+       var testNodes = getElementList();
+       var SyntaxCheck1 = returned_syntaxCheck();
+       console.log("does it reach here?")
+       console.log(SyntaxCheck1);
+       expect(SyntaxCheck1).to.equal(true);
+       done();
+     })
+  })
+
+  it('Syntax Check with ++ constraint links and a single and link', function (done) {
+     source_syntax6a.getData(function(sourceData){
+       var analysis = new InputAnalysis();
+       var links = new InputLink();
+       var js_object = {};
+       var js_links = getLinks();
+       js_object.analysis = getElementList(analysis);
+       graph.fromJSON(sourceData);
+       this.graphList = graph.getElements();
+       this.links = graph.getLinks();
+       console.log(getElementList());
+       console.log(getLinks());
+       var testLink = getLinks();
+       var testNodes = getElementList();
+       var SyntaxCheck1 = returned_syntaxCheck();
+       console.log("does it reach here?")
+       console.log(SyntaxCheck1);
+       expect(SyntaxCheck1).to.equal(true);
+       done();
+     })
+  })
+
+  it('Syntax Check with ++ constraint links and a single or link', function (done) {
+     source_syntax6b.getData(function(sourceData){
+       var analysis = new InputAnalysis();
+       var links = new InputLink();
+       var js_object = {};
+       var js_links = getLinks();
+       js_object.analysis = getElementList(analysis);
+       graph.fromJSON(sourceData);
+       this.graphList = graph.getElements();
+       this.links = graph.getLinks();
+       console.log(getElementList());
+       console.log(getLinks());
+       var testLink = getLinks();
+       var testNodes = getElementList();
+       var SyntaxCheck1 = returned_syntaxCheck();
+       console.log("does it reach here?")
+       console.log(SyntaxCheck1);
+       expect(SyntaxCheck1).to.equal(true);
+       done();
+     })
+  })
+  it('Syntax Check with -- constraint links and a single and link', function (done) {
+     source_syntax8a.getData(function(sourceData){
+       var analysis = new InputAnalysis();
+       var links = new InputLink();
+       var js_object = {};
+       var js_links = getLinks();
+       js_object.analysis = getElementList(analysis);
+       graph.fromJSON(sourceData);
+       this.graphList = graph.getElements();
+       this.links = graph.getLinks();
+       console.log(getElementList());
+       console.log(getLinks());
+       var testLink = getLinks();
+       var testNodes = getElementList();
+       var SyntaxCheck1 = returned_syntaxCheck();
+       console.log("does it reach here?")
+       console.log(SyntaxCheck1);
+       expect(SyntaxCheck1).to.equal(true);
+       done();
+     })
+  })
+
+  it('Syntax Check with -- constraint links and a single or link', function (done) {
+     source_syntax8b.getData(function(sourceData){
+       var analysis = new InputAnalysis();
+       var links = new InputLink();
+       var js_object = {};
+       var js_links = getLinks();
+       js_object.analysis = getElementList(analysis);
+       graph.fromJSON(sourceData);
+       this.graphList = graph.getElements();
+       this.links = graph.getLinks();
+       console.log(getElementList());
+       console.log(getLinks());
+       var testLink = getLinks();
+       var testNodes = getElementList();
+       var SyntaxCheck1 = returned_syntaxCheck();
+       console.log("does it reach here?")
+       console.log(SyntaxCheck1);
+       expect(SyntaxCheck1).to.equal(true);
+       done();
+     })
+  })
+});
 /*
 describe('Read file', function() {
 
