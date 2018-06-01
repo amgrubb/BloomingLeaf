dd4a693c42823fdd8c3e2f7c54fa35183a35f207
Null7 <woran.gao@mail.utoronto.ca>
2018-06-01 13:23:31 -0400

    Deleted code related to .leaf file

--- leaf-ui/scripts/main.js
+++ leaf-ui/scripts/main.js
@@ -727,35 +727,7 @@ function loadAnalysis(analysisResults){
 	updateSlider(currentAnalysis, false);
 }
 
-//function loadAnalysis(analysisResults, analysisType, queryNum){
-//	var type;
-//	if (analysisType == "2"){
-//		type = "Forward";
-//  }else if (analysisType == "3"){
-//		type = "Backward";
-//	}else if (analysisType == "6"){
-//		type = "Leaf Sim";
-//	}else if (analysisType == "7"){
-//		type = "Stochastic";
-//	}else if (analysisType == "11"){
-//		type = "CSP";
-//  }else if (analysisType == "12"){
-//		type = "CSP History";
-//	}else if (analysisType == null){
-//		type = "Unknown";
-//	}
-//
-//	if(queryNum == 1){
-//		type += " A<B";
-//	}else if (queryNum == 2){
-//		type += " B<A";
-//	}else if (queryNum == 3){
-//		type += " A=B";
-//	}
-//
-//	currentAnalysis = new analysisObject.initFromBackEnd(analysisResults, type)
-//	updateSlider(currentAnalysis, false);
-//}
+
 
 
 // ----------------------------------------------------------------- //
@@ -1465,7 +1437,7 @@ $('#btn-load').on('click', function(){
 	$('#loader').click();
 });
 
-//Universally increase or decrease font size
+// Increase font size
 $('#btn-fnt-up').on('click', function(){
 	var elements = graph.getElements();
 	for (var i = 0; i < elements.length; i++){
@@ -1475,6 +1447,7 @@ $('#btn-fnt-up').on('click', function(){
 	}
 });
 
+// Decrease font size
 $('#btn-fnt-down').on('click', function(){
 	var elements = graph.getElements();
 	for (var i = 0; i < elements.length; i++){
@@ -1484,7 +1457,7 @@ $('#btn-fnt-down').on('click', function(){
 	}
 });
 
-//Default font size
+// Default font size
 $('#btn-fnt').on('click', function(){
 	var elements = graph.getElements();
 	for (var i = 0; i < elements.length; i++){
@@ -1492,8 +1465,6 @@ $('#btn-fnt').on('click', function(){
 	}
 });
 
-//Save in .leaf format
-$('#btn-save-leaf').on('click', saveLeaf);
 
 //Simulator
 loader = document.getElementById("loader");
@@ -1529,93 +1500,6 @@ reader.onload = function(){
 };
 
 
-// ----------------------------------------------------------------- //
-// Leaf file generation
-
-// Sends current graph to backend for analysis
-function toBackEnd(simulationType, stepVal, epochVal, queryValA, queryValB){
-	var leafLines = simulationType + "\t" + stepVal + "\t" + epochVal + "\n";
-	leafLines += generateLeafFile();
-
-  var queryLines = []
-	if(queryValA && queryValB){
-		var cellA = queryObject.cellA.model.attributes.elementid;
-		var cellB = queryObject.cellB.model.attributes.elementid;
-		var l1 = "Q\t" + cellA + "\t" + queryValA + "\t<\t" + cellB + "\t" + queryValB + "\n";
-		var l2 = "Q\t" + cellB + "\t" + queryValB + "\t<\t" + cellA + "\t" + queryValA + "\n";
-		var l3 = "Q\t" + cellA + "\t" + queryValA + "\t=\t" + cellB + "\t" + queryValB + "\n";
-		queryLines = [l1, l2, l3];
-	}
-
-  var cspHistoryLines = "";
-	if (simulationType == "12"){
-    var cspHistoryLines = "H\t1\t" + currentAnalysis.numOfElements + "\t" + currentAnalysis.timeScale + "\n";
-    for (i = 0; i < currentAnalysis.elements.length; i++){
-      for(j = 0; j < currentAnalysis.elements[i].length; j++)
-        cspHistoryLines += currentAnalysis.elements[i][j] + "\t";
-      cspHistoryLines += "\n";
-    }
-  }
-
-  var queryNum
-  (queryLines.length == 0) ? queryNum = -1 : queryNum = 0
-  postData(simulationType, leafLines, queryLines, cspHistoryLines, queryNum)
-}
-
-function postData(simulationType, leafLines, queryLines, cspHistoryLines, queryNum){
-	console.log("simulationType");
-	console.log(simulationType);
-	console.log("leafLines");
-	console.log(leafLines);
-	console.log("queryLines");
-	console.log(queryLines);
-	console.log("cspHistoryLines");
-	console.log(cspHistoryLines);
-	console.log("queryNum");
-	console.log(queryNum);
-
-	var data = {};
-	if(queryNum == -1){
-		data.toUpload = leafLines + cspHistoryLines;
-		data.simgraph = JSON.stringify(graph.toJSON());
-  }else{
-		data.toUpload = leafLines + queryLines[queryNum] + cspHistoryLines;
-		data.simgraph = JSON.stringify(graph.toJSON());
-		queryNum++;
-  }
-  console.log('data');
-  console.log(data);
-	//Send data to backend for analysis
-	$.post("./cgi-bin/handleupload2.cgi", data, function(results, status){
-		if(status == "success"){
-			analysisResults = results.split("\n");
-			if (isNaN(parseInt(analysisResults[0]))){
-				alert("Sorry Dave, We could not process your model")
-				return
-			}
-			loadAnalysis(analysisResults, simulationType, queryNum);
-
-			var currentValueLimit = parseInt(sliderObject.sliderElement.noUiSlider.get());
-			var sliderMax = currentValueLimit + currentAnalysis.timeScale;
-			sliderObject.sliderElement.noUiSlider.set(sliderMax);
-
-			if ((queryNum != -1) && (queryNum < queryLines.length))
-				postData(simulationType, leafLines, queryLines, cspHistoryLines, queryNum)
-		}else{
-			alert("Sorry Dave, We could not process your model")
-		}
-	});
-}
-
-//Save in a .leaf format
-function saveLeaf(){
-	var datastring = generateLeafFile();
-	var name = window.prompt("Please enter a name for your file. \nIt will be saved in your Downloads folder. \n.leaf will be added as the file extension.", "<file name>");
-	if (name){
-		var fileName = name + ".leaf";
-		download(fileName, datastring);
-	}
-}
 
 //Helper function to download saved graph in JSON format
 function download(filename, text) {
@@ -1631,198 +1515,6 @@ function download(filename, text) {
 }
 
 
-// Generates file needed for backend analysis
-function generateLeafFile(){
-
-	//Step 0: Get elements from graph.
-	var all_elements = graph.getElements();
-	var savedLinks = [];
-	var savedConstraints = [];
-
-	if (linkMode == "View"){
-		savedConstraints = graph.intensionConstraints;
-		var links = graph.getLinks();
-	    links.forEach(function(link){
-	        if(!isLinkInvalid(link)){
-				if (link.attr('./display') != "none")
-	        		savedLinks.push(link);
-	        }
-	        else{link.remove();}
-	    });
-	}else if (linkMode == "Constraints"){
-		savedLinks = graph.links;
-		var betweenIntensionConstraints = graph.getLinks();
-	    betweenIntensionConstraints.forEach(function(link){
-			var linkStatus = link.attributes.labels[0].attrs.text.text.replace(/\s/g, '');
-	        if(!isLinkInvalid(link) && (linkStatus != "constraint") && (linkStatus != "error")){
-				if (link.attr('./display') != "none")
-					savedConstraints.push(link);
-	        }
-	        else{link.remove();}
-	    });
-	}
-
-	//Step 1: Filter out Actors
-	var elements = [];
-	var actors = [];
-	for (var e1 = 0; e1 < all_elements.length; e1++){
-		if (!(all_elements[e1] instanceof joint.shapes.basic.Actor)){
-			elements.push(all_elements[e1]);
-		}
-		else{
-			actors.push(all_elements[e1]);
-		}
-	}
-
-	//save elements in global variable for slider, used for toBackEnd funciton only
-	graph.allElements = elements;
-	graph.elementsBeforeAnalysis = elements;
-
-	var datastring = actors.length + "\n";
-	//print each actor in the model
-	for (var a = 0; a < actors.length; a++){
-		var actorId = a.toString();
-		while (actorId.length < 3){ actorId = "0" + actorId;}
-		actorId = "a" + actorId;
-		actors[a].prop("elementid", actorId);
-		datastring += ("A\t" + actorId + "\t" + actors[a].attr(".name/text") + "\t" + (actors[a].prop("actortype") || "A") + "\n");
-	}
-
-
-	// Step 2: Print each element in the model
-
-	// conversion between values used in Element Inspector with values used in backend
-	var satValueDict = {
-		"unknown": 5,
-		"satisfied": 3,
-		"partiallysatisfied": 2,
-		"partiallydenied": 1,
-		"denied": 0,
-		"conflict": 4,
-		"none": 6
-	}
-	datastring += elements.length + "\n";
-	for (var e = 0; e < elements.length; e++){
-		//var id = e.toString();
-		//while (id.length < 4){ id = "0" + id;}
-		//elements[e].prop("elementid", id);
-		var elementID = e.toString();
-		while (elementID.length < 4){ elementID = "0" + elementID;}
-		elements[e].prop("elementid", elementID);
-
-		var actorid = '-';
-		if (elements[e].get("parent")){
-			actorid = (graph.getCell(elements[e].get("parent")).prop("elementid") || "-");
-		}
-		console.log(actorid);
-
-	// Print NT in "core" of tool where time does not exist.
-	//datastring += ("I\t" + actorid + "\t" + elementID + "\t" + (functions[elements[e].attr(".funcvalue/text")] || "NT") + "\t");
-
-	  datastring += ("I\t" + actorid + "\t" + elementID + "\t");
-		if (elements[e] instanceof joint.shapes.basic.Goal)
-		  	datastring += "G\t";
-		else if (elements[e] instanceof joint.shapes.basic.Task)
-		  	datastring += "T\t";
-		else if (elements[e] instanceof joint.shapes.basic.Softgoal)
-		  	datastring += "S\t";
-		else if (elements[e] instanceof joint.shapes.basic.Resource)
-		  	datastring += "R\t";
-		else
-	  		datastring += "I\t";
-
-	  	var v = elements[e].attr(".satvalue/value")
-
-	  	// treat satvalue as unknown if it is not yet defined
-	  	if((!v) || (v == "none"))
-			v = "none";
-
-		datastring += satValueDict[v];
-		datastring += "\t" + elements[e].attr(".name/text").replace(/\n/g, " ") + "\n";
-	}
-
-
-	//Step 3: Print each link in the model
-	for (var l = 0; l < savedLinks.length; l++){
-		var current = savedLinks[l];
-		var relationship = current.label(0).attrs.text.text.toUpperCase()
-		var source = "-";
-		var target = "-";
-
-		if (current.get("source").id)
-			source = graph.getCell(current.get("source").id).prop("elementid");
-		if (current.get("target").id)
-			target = graph.getCell(current.get("target").id).prop("elementid");
-
-		if (relationship.indexOf("|") > -1){
-			evolvRelationships = relationship.replace(/\s/g, '').split("|");
-			datastring += 'L\t' + evolvRelationships[0] + '\t' + source + '\t' + target + '\t' + evolvRelationships[1] + "\n";
-		}else{
-			datastring += 'L\t' + relationship + '\t' + source + '\t' + target + "\n";
-		}
-	}
-
-	//Step 4: Print the dynamics of the intentions.
-	for (var e = 0; e < elements.length; e++){
-	    var elementID = e.toString();
-	    while (elementID.length < 4){ elementID = "0" + elementID;}
-	    elements[e].prop("elementid", elementID);
-
-	    //datastring += ("I\t" + actorid + "\t" + elementID + "\t" + (functions[elements[e].attr(".funcvalue/text")] || "NT") + "\t");
-	    var f = elements[e].attr(".funcvalue/text");
-	    var funcType = elements[e].attr(".constraints/function");
-	    var funcTypeVal = elements[e].attr(".constraints/lastval");
-	    if  (f == " "){
-	    	datastring += ("D\t" + elementID + "\tNT\n");
-	    }else if (f != "UD"){
-	    	datastring += ("D\t" + elementID + "\t" + f + "\t" + satValueDict[funcTypeVal] + "\n");
-
-	    // user defined constraints
-	    }else{
-			var begin = elements[e].attr(".constraints/beginLetter");
-			var end = elements[e].attr(".constraints/endLetter");
-			var rBegin = elements[e].attr(".constraints/beginRepeat");
-			var rEnd = elements[e].attr(".constraints/endRepeat");
-			datastring += "D\t" + elementID + "\t" + f + "\t" + String(funcTypeVal.length);
-
-			for (var l = 0; l < funcTypeVal.length; l++){
-				if(l == funcTypeVal.length - 1){
-					datastring += "\t" + begin[l] + "\t1\t" + funcType[l] + "\t" + satValueDict[funcTypeVal[l]];
-				}else{
-					datastring += "\t" + begin[l] + "\t" + end[l] + "\t" + funcType[l] + "\t" + satValueDict[funcTypeVal[l]];
-				}
-			}
-
-			// repeating
-			if (elements[e].attr(".constraints/beginRepeat") && elements[e].attr(".constraints/endRepeat")){
-				// to infinity
-				if (rEnd == end[end.length - 1]){
-					datastring += "\tR\t" + rBegin + "\t1";
-				}else{
-					datastring += "\tR\t" + rBegin + "\t" + rEnd;
-				}
-			}else{
-				datastring += "\tN";
-			}
-				datastring += "\n";
-			}
-	}
-
-	//Step 5: Print constraints between intensions.
-	for (var e = 0; e < savedConstraints.length; e++){
-		var c = savedConstraints[e];
-		var type = c.attributes.labels[0].attrs.text.text.replace(/\s/g, '');
-		var source = c.getSourceElement().attributes.elementid;
-		var target = c.getTargetElement().attributes.elementid;
-		var sourceVar = c.attr('.constraintvar/src');
-		var targetVar = c.attr('.constraintvar/tar');
-
-		datastring += ("C\t" + type + "\t" + source + "\t" + sourceVar + "\t" + target + "\t" + targetVar + "\n");
-	}
-
-	//console.log(datastring);
-	return datastring
-}
 
 // ----------------------------------------------------------------- //
 // General javascript for user interaction
