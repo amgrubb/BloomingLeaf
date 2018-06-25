b9970c2998f6dde8f0c82f8ba8e28296b32aae15
David <20326645+xorye@users.noreply.github.com>
2018-06-25 19:10:43 -0400

    Change InputAnalysis' default absTimePts and currentState values

--- leaf-ui/js/object/InputAnalysis.js
+++ leaf-ui/js/object/InputAnalysis.js
@@ -6,13 +6,13 @@ class InputAnalysis {
 		this.action = action; //'singlePath' or 'allNextStates'
 		this.maxAbsTime = $('#max-abs-time').val();
 		this.conflictLevel = $('#conflict-level').val();
-		this.absTimePts = $('#abs-time-pts').val();
-		this.currentState = $('#sliderValue').text();
+		this.numRelTime = $('#num-rel-time').val();
+		this.absTimePts = $('#abs-time-pts').val() ? $('#abs-time-pts').val() : null;
+		this.currentState = $('#sliderValue').text() ? $('#sliderValue').text() : '0';
 		this.initialAssignedEpoch = savedAnalysisData.finalAssignedEpoch ? savedAnalysisData.finalAssignedEpoch : ['0'];
 		this.initialValueTimePoints = savedAnalysisData.finalValueTimePoints ? savedAnalysisData.finalValueTimePoints:
 			['0'];
 		this.elementList = getElementsForAnalysis();
-        this.numRelTime = $('#num-rel-time').val();
 	}
 }
 
