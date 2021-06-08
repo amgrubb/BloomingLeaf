4599ba3375e2eedd58f2e357d9172f9c81044326
Manarnz <83615524+Manarnz@users.noreply.github.com>
2021-06-08 13:22:37 -0400

    Remove NB tag after switching link type

--- leaf-ui/rappid-extensions/LinkInspector.js
+++ leaf-ui/rappid-extensions/LinkInspector.js
@@ -170,11 +170,16 @@ var LinkInspector = Backbone.View.extend({
             
         } else {
             //Verify if it is possible to remove the NB tag from source and target
-            if (!this.hasNBLink(source, this.cell) && this.hasNBTag(source)){
+            if (!this.hasNBLink(source, this.cell) && !this.hasNBTag(source)){
+                
                 source.attr(".funcvalue/text", "");
+                source.attr('.satvalue/text', '');
+                
             }
-            if (!this.hasNBLink(target, this.cell) && this.hasNBTag(target)){
+            if (!this.hasNBLink(target, this.cell) && !this.hasNBTag(target)){
+                
                 target.attr(".funcvalue/text", "");
+                target.attr('.satvalue/text', '');
             }
         }
 
