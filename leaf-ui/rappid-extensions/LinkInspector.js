971851f35a0952cccc8bd20781337edb9dc1db12
dodo kung <ckung@smith.edu>
2021-06-08 13:55:43 -0400

    Evolving relationships that start with no relationship can choose second option now

--- leaf-ui/rappid-extensions/LinkInspector.js
+++ leaf-ui/rappid-extensions/LinkInspector.js
@@ -127,7 +127,7 @@ var LinkInspector = Backbone.View.extend({
         } else {
             this.$el.html(_.template(this.template)());
             $('#constant-links').val(values[0].trim());
-            this.evolvingRelations = false;
+            this.evolvingRelations = false; //redundant????
         }
     },
 
@@ -170,11 +170,13 @@ var LinkInspector = Backbone.View.extend({
             
         } else {
             //Verify if it is possible to remove the NB tag from source and target
-            if (!this.hasNBLink(source, this.cell) && this.hasNBTag(source)){
+            if (!this.hasNBLink(source, this.cell) && !this.hasNBTag(source)){
                 source.attr(".funcvalue/text", "");
+                source.attr('.satvalue/text', '');
             }
-            if (!this.hasNBLink(target, this.cell) && this.hasNBTag(target)){
-                target.attr(".funcvalue/text", "");
+            if (!this.hasNBLink(target, this.cell) && !this.hasNBTag(target)){
+                target.attr(".funcvalue/text", ""); 
+                target.attr('.satvalue/text', '');
             }
         }
 
@@ -224,9 +226,8 @@ var LinkInspector = Backbone.View.extend({
         //Enable the end select
         if (begin == "no") {
             this.setSelectValues('#link-type-end', 'Evolving');
-            $("#link-type-end").prop('disabled', true);
-            $("#link-type-end").css("background-color", "gray");
-            $("#repeat-error").text("");
+            $("#link-type-end").prop('disabled', false);
+            $("#link-type-end").css("background-color", "");
         } else if(begin == "and" || begin == "or") {
             this.setSelectValues('#link-type-end', 'A');
 
@@ -253,7 +254,7 @@ var LinkInspector = Backbone.View.extend({
 
         } else {
             this.setSelectValues('#link-type-end', "B");
-            $("#link-type-end").prop('disabled', '');
+            $("#link-type-end").prop('disabled', ''); 
             $("#link-type-end").css("background-color","");
         }
     },
@@ -273,7 +274,7 @@ var LinkInspector = Backbone.View.extend({
             '.marker-source': {'d': '0'},
             '.marker-target': {stroke: '#000000', "d": 'M 10 0 L 0 5 L 10 10 L 0 5 L 10 10 L 0 5 L 10 5 L 0 5'}
         });
-        this.cell.label(0, {position: 0.5, attrs: {text: {text: begin + " | " + end}}});
+        this.cell.label(0, {position: 0.5, attrs: {text: {text: begin + " | " + end}}}); //do we need the codes then in lines 239 and 245????
 
         // save into link object
         this.link.linkType = begin;
@@ -365,7 +366,7 @@ var LinkInspector = Backbone.View.extend({
         if (selector == "link-type-end") {
             var dupVal = $('#link-type-begin').val();
             $("select#link-type-end option").filter("[value='" + dupVal + "']").remove();
-        }
+        }//feels very inefficient????
     },
 
     clear: function(){
