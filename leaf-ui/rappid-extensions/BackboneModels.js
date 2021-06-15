3f18444ba0658f88dc9173ab09d540f2f56cdeaa
Yesugen <ybaatartogtokh@smith.edu>
2021-06-15 17:33:13 -0400

    Added backbone model representations for UserEvaluation and Constraint

--- leaf-ui/rappid-extensions/BackboneModels.js
+++ leaf-ui/rappid-extensions/BackboneModels.js
@@ -1,1 +1,30 @@
 /** This file contains backbone model representations of the original model objects - WIP */
+
+/** 
+ * Backbone for UserEvaluation 
+ * */ 
+var UserEvaluationModel = Backbone.Model.extend({  
+    //Take in properties
+    initialize: function(options){ //Named arguments and passes in parameter
+        this.intentionID = options.intentionID; 
+        this.absTime = options.absTime;
+        this.evaluationValue = options.evaluationValue;
+    }
+});
+
+/** 
+ * Backbone for Constraint 
+ * */
+var ConstraintModel = Backbone.Model.extend({
+    initialize: function(options){ //Named arguments and passes in parameter
+        this.constraintType = options.constraintType;
+        this.constraintSrcID = options.constraintSrcID;
+        this.constraintSrcEB = options.constraintSrcEB;
+        this.constraintDestID = options.constraintDestID;
+        this.constraintDestEB = options.constraintDestEB;
+        this.absoluteValue = -1;
+    }
+});
+
+//var intentionEval = new UserEvaluationModel({intentionID:intention.nodeID},{absTime:'0'}, {evaluationValue:'(no value)'});
+//var constraint = new ConstraintModel({constraintType:type},{constraintSrcID:nodeID1}, {constraintSrcEB:epoch1}, {constraintDestID: nodeID2}, {constraintDestEB:epoch2});
