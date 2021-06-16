84a4565f5a645b561e96468ec37ae086fe7f7528
YesugenB <59470488+YesugenB@users.noreply.github.com>
2021-06-16 16:05:38 -0400

    Moved onclick event to onFunctions.js (#412)
    * Moved onclick event to onFunctions.js, copy paste functions work
    
    * Moved keyboard functions and deleted old file
    
    * removed keyboardFunctions.js from index.html

--- leaf-ui/js/onFunctionsBothWindows.js
+++ leaf-ui/js/onFunctionsBothWindows.js
@@ -64,3 +64,69 @@ function defaultFont(pPaper) {
 }
 
 //end nav bar functions
+
+/**
+ * Set up Ctrl+c and Ctrl+v shortcut for macOS
+ *  
+ */
+ var clipboard = new joint.ui.Clipboard();
+ // Check if the browser is on Mac
+ var macOS = navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)?true:false;
+ if(macOS) {
+     KeyboardJS.on('command + c, ctrl + c', function() {
+         // Copy all selected elements and their associatedf links.
+         clipboard.copyElements(selection, graph, { translate: { dx: 20, dy: 20 }, useLocalStorage: true });
+     });
+     KeyboardJS.on('command + v, ctrl + v', function() {
+         clipboard.pasteCells(graph);
+ 
+         selectionView.cancelSelection();
+ 
+         clipboard.pasteCells(graph, { link: { z: -1 }, useLocalStorage: true });
+ 
+         // Make sure pasted elements get selected immediately. This makes the UX better as
+         // the user can immediately manipulate the pasted elements.
+         clipboard.each(function(cell) {
+             if (cell.get('type') === 'link') return;
+ 
+             // Push to the selection not to the model from the clipboard but put the model into the graph.
+             // Note that they are different models. There is no views associated with the models
+             // in clipboard.
+             selection.add(graph.get('cells').get(cell.id));
+         });
+ 
+         selection.each(function(cell) {
+         selectionView.createSelectionBox(paper.findViewByModel(cell));
+         });
+     });
+ 
+ } else {
+ 
+     KeyboardJS.on('ctrl + c', function() {
+         // Copy all selected elements and their associatedf links.
+         clipboard.copyElements(selection, graph, { translate: { dx: 20, dy: 20 }, useLocalStorage: true });
+     });
+     KeyboardJS.on('ctrl + v', function() {
+         clipboard.pasteCells(graph);
+ 
+         selectionView.cancelSelection();
+ 
+         clipboard.pasteCells(graph, { link: { z: -1 }, useLocalStorage: true });
+ 
+         // Make sure pasted elements get selected immediately. This makes the UX better as
+         // the user can immediately manipulate the pasted elements.
+         clipboard.each(function(cell) {
+             if (cell.get('type') === 'link') return;
+ 
+             // Push to the selection not to the model from the clipboard but put the model into the graph.
+             // Note that they are different models. There is no views associated with the models
+             // in clipboard.
+             selection.add(graph.get('cells').get(cell.id));
+         });
+ 
+         selection.each(function(cell) {
+         selectionView.createSelectionBox(paper.findViewByModel(cell));
+         });
+     });
+ 
+ }
