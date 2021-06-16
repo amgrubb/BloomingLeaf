cc560673bd90f8878ac6776e9e286c0a451f2875
angelac2023 <achu91@smith.edu>
2021-06-16 11:59:33 -0400

    Changed parameters in initialize function to single option parameter

--- leaf-ui/rappid-extensions/BackboneModels.js
+++ leaf-ui/rappid-extensions/BackboneModels.js
@@ -7,7 +7,7 @@ var FuncSegmentModel = Backbone.Model.extend({
 
     // initialize function - could also save all of the input parameters into one variable 
     // 'options' and then use that like Yesuegen 
-    initialize: function (funcType, funcX, funcStart, funcStop) {
+    initialize: function (options) {
         this.funcType = options.funcType;
         this.funcX = options.funcX;
         this.funcStart = options.funcStart;
@@ -15,13 +15,15 @@ var FuncSegmentModel = Backbone.Model.extend({
     },
 });
 
+//RepFuncSegmentMOdel from RepFuncSegment class in modelObjects.js
 var RepFuncSegmentModel = Backbone.Model.extend({
     idAttribute: "uid",
 
-    initialize: function(functionSegList, repNum, absTime) { 
-        this.functionSegList = functionSegList; 
-        this.repNum = repNum; 
-        this.absTime = absTime; 
+    initialize: function(options) { 
+        this.functionSegList = options.functionSegList; 
+        this.repNum = options.repNum; 
+        this.absTime = options.absTime; 
     }
 
 });     
+
