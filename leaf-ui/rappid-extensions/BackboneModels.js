6e72c31eb0217737cd5767895e44bf6851ab4ca6
YesugenB <59470488+YesugenB@users.noreply.github.com>
2021-06-17 14:11:47 -0400

    Added backbone model representations for UserEvaluation and Constraint (#420)
    * Added backbone model representations for UserEvaluation and Constraint
    
    * Deleted commented lines
    
    * Made name changes and defaults
    
    * added this.type
    
    * Changed destRefTP name.
    
    * Update BackboneModels.js
    
    * removed defaults
    
    Co-authored-by: A. M. Grubb <amgrubb@smith.edu>

--- leaf-ui/rappid-extensions/BackboneModels.js
+++ leaf-ui/rappid-extensions/BackboneModels.js
@@ -1,6 +1,4 @@
 /** This file contains backbone model representations of the original model objects - WIP */
-
-
 // FuncSegmentModel from FuncSegment class in modelObjects.js
 var FunctionSegmentBBM = Backbone.Model.extend({
     idAttribute: "uid",
@@ -27,3 +25,42 @@ var RepFunctionSegmentBBM = Backbone.Model.extend({
     }
 
 });     
+
+/** 
+ * Backbone for UserEvaluation 
+ * */ 
+var UserEvaluationBBM = Backbone.Model.extend({  
+    //Take in properties
+    initialize: function(options){ //Named arguments and passes in parameter
+        this.intentionID = options.intentionID; 
+        this.absTP = options.absTP;
+        this.assignedValue = options.assignedValue;
+    }
+});
+
+/** 
+ * Backbone for Constraint 
+ * */
+var ConstraintBBM = Backbone.Model.extend({
+    initialize: function(options){ //Named arguments and passes in parameter
+        this.type = options.type;       // Options are '=', '<', and '<='
+        this.srcID = options.srcID;
+        this.srcRefTP = options.srcRefTP;
+        this.destID = options.destID;
+        this.destRefTP = options.destRefTP;
+        /* Absolute time points are only used with the '=' type of operator.
+         *   If a timepoint is not given -1 should be assigned as the default value.
+         * */   
+        if (this.absTP != 'undefined') {
+            if (this.type  == '<' || this.type == '<='){
+                this.absTP = null; 
+            }else{
+                this.absTP = -1;
+            }
+        }else{
+            this.absTP = this.absTP;
+        }
+ 
+    },
+});
+
