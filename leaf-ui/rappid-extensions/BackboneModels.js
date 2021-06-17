0a235a9ccd282f2666cc749ffa259252943c3f8e
i-m-foster <72589318+i-m-foster@users.noreply.github.com>
2021-06-17 14:03:31 -0400

    Created FuncSegment and RepFuncSegment backbone models in BackboneModels.js (#421)
    * made FuncSegment and RepFuncSegment backbone models
    
    * Changed parameters in initialize function to single option parameter
    
    * Updated variable names
    
    * Update RepFuncSegment model name
    
    * Update BackboneModels.js
    
    Co-authored-by: angelac2023 <achu91@smith.edu>
    Co-authored-by: A. M. Grubb <amgrubb@smith.edu>

--- leaf-ui/rappid-extensions/BackboneModels.js
+++ leaf-ui/rappid-extensions/BackboneModels.js
@@ -1,1 +1,29 @@
 /** This file contains backbone model representations of the original model objects - WIP */
+
+
+// FuncSegmentModel from FuncSegment class in modelObjects.js
+var FunctionSegmentBBM = Backbone.Model.extend({
+    idAttribute: "uid",
+
+    // Initialize function - save all of the input parameters into 'option' variable
+    // And then use 'option' when intitializing the function  
+    initialize: function (options) {
+        this.type = options.type;
+        this.refEvaluationValue = options.refEvaluationValue;
+        this.startTP = options.startTP;
+        this.stopTP = options.stopTP;
+    },
+});
+
+//RepFuncSegmentMOdel from RepFuncSegment class in modelObjects.js
+var RepFunctionSegmentBBM = Backbone.Model.extend({
+    idAttribute: "uid",
+
+    initialize: function(options) { 
+        // this functionSegList is an array of all of the FunctionSegmentBBMs in this BBM
+        this.functionSegList = options.functionSegList; 
+        this.repNum = options.repNum; 
+        this.absRepTime = options.absRepTime; 
+    }
+
+});     
