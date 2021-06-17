8fbdea51289296d8b4921f5fb34617c89c4910eb
Manarnz <83615524+Manarnz@users.noreply.github.com>
2021-06-17 14:55:10 -0400

    Cleaned up the code

--- leaf-ui/rappid-extensions/BackboneModels.js
+++ leaf-ui/rappid-extensions/BackboneModels.js
@@ -4,45 +4,23 @@ var LinkCollection = Backbone.Collection.extend({
 })
 var LinkBB = Backbone.Model.extend({
     idAttribute: "uid",
-    /**
-     * {String} linkType
-     *   Type of the link. ex: 'AND', 'OR', 'NO', etc.
-     * {String} linkSrcID
-     *   ID for the source of the link. ex: '0000'
-     * {String} linkDestID
-     *   ID for the destination of the link. ex: '0001'
-     * {Number} absoluteValue
-     *   TODO ex. -1, 0,...,n
-     */
-     initialize: function(options){
+    
+    initialize: function(options){
         
-        this.linkSrcID = options.linkSrcID;//isn't the same ID as the source? Maybe there is a way
+        this.linkSrcID = options.linkSrcID;
         this.linkType =  'AND';
         this.absoluteValue =  -1;
         this.linkID = this.createID();
         this.numOfCreatedInstances = new LinkCollection([]);
-     },
+    },
     
     defaults: {
         type: 'Intention',
         postType: null,
         linkDestID: null,
-        //linkID: createID(),
         numOfCreatedInstances: new LinkCollection([]),
-        //these are for the other branch 
-        //evolving: false,
-        //selected: false,
-        //relationship: 'Constant',
     },
-    checkType: function(){
-        if (this.get('type') == 'Actor'){
-            this.set('evolving', false);
-            this.set('selected', false);
-        }
-    },
-    isEvolvingRelationship: function() {
-        return this.postType != null;
-    },
+
     createID: function(){
         var id = this.get('numOfCreatedInstances').length.toString();
         while (id.length < 4){
