2c4b9fd7aefa34e502ca1945a185e3fefab80071
YesugenB <59470488+YesugenB@users.noreply.github.com>
2021-06-17 10:22:39 -0400

    Cleanup displayAnalysis (#422)
    * Added backbone model representations for UserEvaluation and Constraint
    
    * Deleted commented lines
    
    * Removed dead functions and commented out lines
    
    * Deleted more functions
    
    * Removed extra file
    
    * Deleted refreshAnalysisUI() switchResults() and switchSelectedShadingResult(). Made issue for refreshAnalysisUI
    
    * Added some TODOs
    
    Co-authored-by: leshghi <43756305+leshghi@users.noreply.github.com>

--- leaf-ui/js/onFunctions.js
+++ leaf-ui/js/onFunctions.js
@@ -158,41 +158,6 @@ $('#load-sample').on('click', function() {
 });
 */
 
-/** Analysis Configuration Sidebar */
-
-// /**
-//  * Adds a new AnalysisConfig
-//  */
-//  $('#addConfig').on('click', function(){
-//     addNewAnalysisConfig();
-// });
-
-// /**
-//  * Allows user to rename configuration element on doubleclick
-//  */
-// $(document).on('dblclick', '.config-elements', function(e){rename(e.target /** Config element */)});
-
-// /**
-//  * Switches UI to clicked configuration element
-//  */
-// $(document).on('click', '.config-elements', function(e){switchConfigs(e.target.closest('.analysis-configuration') /** Config element */)});
-   
-// /**
-//  * Toggles results dropdown menu on click of dropdown arrow
-//  */
-// $(document).on('click','.dropdown-button', function(e){toggleDropdown(e.target.closest('.analysis-configuration') /** Config element */)});
-
-// /**
-//  * Deletes configuration from UI and analysisMap on click of delete button
-//  */
-// $(document).on('click','.deleteconfig-button', function(e){removeConfiguration(e.target.closest('.analysis-configuration') /** Config element */)});
-
-// /**
-//  * Switches to clicked result and it's corresponding configuration in UI
-//  */
-// $(document).on('click', '.result-elements', function(e){switchResults(e.target /** Result element */, e.target.closest('.analysis-configuration') /** Config element */)})
-
-
 /**
  * Trigger when unassign button is pressed. 
  * Change the assigned time of the node/link in the same row to none
@@ -461,22 +426,6 @@ function switchToAnalysisMode() {
 	clearInspector();
 	
 	removeHighlight();
-
-    // // clear results if changed model during modeling mode
-    // let modelChanged = !(JSON.stringify(previousModel) === JSON.stringify(model));
-    // if (modelChanged){
-    //     clearResults();
-    // }
-
-    // // Checks if the user assignments list has changed since last switching to Assignments mode
-    // // If so, update UAL for all configs and then update defaultUAL 
-    // if(analysisRequest.userAssignmentsList !== defaultUAL){
-    //     for(let config of analysisMap.values()){
-    //         config.updateUAL(analysisRequest.userAssignmentsList);
-    //     }
-    //     defaultUAL = [];
-    //     analysisRequest.userAssignmentsList.forEach(uAL => defaultUAL.push(uAL));
-    // }
     
     $('#config').append(configInspector.el);
     configInspector.render();
@@ -657,8 +606,6 @@ $('#btn-clear-all').on('click', function(){
     graph.clear();
     // reset to default analysisRequest
     model.removeAnalysis();
-    // clear analysis sidebar
-    clearAnalysisConfigSidebar();
     // remove all configs from analysisMap
     analysisMap.clear();
 	// Delete cookie by setting expiry to past date
@@ -710,21 +657,16 @@ $('#btn-clear-cycle').on('click',function(){
 });
 
 $('#btn-clear-analysis').on('click', function() {
+    // TODO: Re-Implement for backbone view
+    
     // reset to default analysisRequest while preserving userAssignmentsList
     resetToDefault();
-    // clear analysis sidebar
-    clearAnalysisConfigSidebar();
-    // remove all configs from analysisMap
-    analysisMap.clear();
-	// add back first default analysis config
-    addFirstAnalysisConfig();
     // reset graph to initial values
     revertNodeValuesToInitial();
 });
 
 $('#btn-clear-results').on('click', function() {
-    clearResults();
-    refreshAnalysisUI();
+    // TODO: Re-implement for backbone view
 });
 
 // Open as SVG
