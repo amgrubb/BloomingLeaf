f39654354cc1476e9d906c3ea7595740df51cf8d
A. M. Grubb <amgrubb@smith.edu>
2021-07-07 10:19:49 -0400

    Added basic cell option.

---
+++ leaf-analysis/src/gson_classes/InputCell.java
@@ -0,0 +1,10 @@
+package gson_classes;
+
+public class InputCell {
+	private String type;
+	private String id;
+	private String angle;
+	private String z;
+	
+	
+}
--- leaf-analysis/src/gson_classes/InputGraph.java
+++ leaf-analysis/src/gson_classes/InputGraph.java
@@ -1,19 +1,20 @@
 package gson_classes;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 /**
- * This class is responsible to receive the frontend JSON, so it doesn't have any behavior (POJO).
- * It is split in two attributes, the data required for the type of analysis and the graph model.
- * IMPORTANT: The attributes in this class must be correlated (same name and type) with the JSON attributes.
- * @author marcel
- * @email marcel.serikawa@gmail.com
+ * This class is responsible to receive the frontend JSON, so it doesn't have any behavior.
+ * IMPORTANT: The attributes in this class must be correlated (same name and type) with 
+ * the JSON attributes.
  */
 public class InputGraph {
 	
 	private String maxAbsTime;
 	private ArrayList<String> absTimePtsArr;
 	private ArrayList<String> constraints;
+	private List<InputCell> cells;
 	
 //	InputModel model = new InputModel();
 	
