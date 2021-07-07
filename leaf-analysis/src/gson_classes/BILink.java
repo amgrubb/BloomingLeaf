bb67173581130061a6c14a5c1770c6f3364a1fa3
Manarnz <83615524+Manarnz@users.noreply.github.com>
2021-07-07 15:28:02 -0400

    Created BIConstraint and BILink (from BackboneModelsOther.js)

---
+++ leaf-analysis/src/gson_classes/BILink.java
@@ -0,0 +1,12 @@
+package gson_classes;
+
+public class BILink {
+    private Attributes attributes;
+	
+	private class Attributes {
+		Integer absTime;
+        Boolean evolving;
+        String linkType;
+        String postType;
+	}
+}
