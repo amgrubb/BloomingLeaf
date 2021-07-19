8c72077737fb1482c9b7c2f041bb9df6b227cf1f
A. M. Grubb <amgrubb@smith.edu>
2021-07-19 13:00:11 -0400

    tidied up formatting.

--- leaf-ui/rappid-extensions/BackboneModelsOther.js
+++ leaf-ui/rappid-extensions/BackboneModelsOther.js
@@ -10,12 +10,12 @@
 /** 
  * Backbone Model of Actor
  */
- var ActorBBM = Backbone.Model.extend({
-    initialize: function(options){
+var ActorBBM = Backbone.Model.extend({
+    initialize: function (options) {
         _.extend({}, this.defaults, options);
     },
 
-    defaults: function() {
+    defaults: function () {
         return {
             type: 'A',
             actorName: 'Actor'
@@ -27,15 +27,15 @@
 /** 
  * Backbone Model of Link.
  */
- var LinkBBM = Backbone.Model.extend({  
-    initialize: function(options){
+var LinkBBM = Backbone.Model.extend({
+    initialize: function (options) {
         _.extend({}, this.defaults, options);
     },
 
-    defaults: function() {
+    defaults: function () {
         return {
             displayType: 'element',     // TODO: should this be changed to 'link'?
-            linkType:  'and',
+            linkType: 'and',
             postType: null,
             absTime: -1,
             evolving: false,
@@ -48,11 +48,11 @@
  * Backbone Model of Constraints
  */
 var ConstraintBBM = Backbone.Model.extend({
-    initialize: function(options){
+    initialize: function (options) {
         _.extend({}, this.defaults, options)
     },
 
-    defaults: function() {
+    defaults: function () {
         return {
             type: null,           // Options are '=', '<', and '<='
             srcID: null,
@@ -62,7 +62,7 @@ var ConstraintBBM = Backbone.Model.extend({
             /** 
              * Absolute time points are only used with the '=' type of operator.
              *  If a timepoint is not given -1 should be assigned as the default value.
-             */   
+             */
             absTP: -1,
         }
     },
@@ -71,10 +71,10 @@ var ConstraintBBM = Backbone.Model.extend({
      * Checks if type of constraint is null
      * If so, constraint has not been fully assigned
      */
-    isComplete: function(){
-        return (!(this.get('type') == null) && 
-                !(this.get('srcID') == null) &&
-                !(this.get('destID') == null));
+    isComplete: function () {
+        return (!(this.get('type') == null) &&
+            !(this.get('srcID') == null) &&
+            !(this.get('destID') == null));
     },
 
 });
