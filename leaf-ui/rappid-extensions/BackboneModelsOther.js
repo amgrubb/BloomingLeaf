10f7c0eb0848928b45c2a88fbd03358de2e9dfd9
Manarnz <83615524+Manarnz@users.noreply.github.com>
2021-07-19 14:01:05 -0400

    Fixed issue #492 (#498)
    * Fixed issue #492
    
    * tidied up formatting.
    
    Co-authored-by: A. M. Grubb <amgrubb@smith.edu>

--- leaf-ui/rappid-extensions/BackboneModelsOther.js
+++ leaf-ui/rappid-extensions/BackboneModelsOther.js
@@ -10,14 +10,16 @@
 /** 
  * Backbone Model of Actor
  */
- var ActorBBM = Backbone.Model.extend({
-    initialize: function(options){
+var ActorBBM = Backbone.Model.extend({
+    initialize: function (options) {
         _.extend({}, this.defaults, options);
     },
 
-    defaults: {
-        type: 'A',
-        actorName: 'Actor'
+    defaults: function () {
+        return {
+            type: 'A',
+            actorName: 'Actor'
+        }
     },
 });
 
@@ -25,17 +27,19 @@
 /** 
  * Backbone Model of Link.
  */
- var LinkBBM = Backbone.Model.extend({  
-    initialize: function(options){
+var LinkBBM = Backbone.Model.extend({
+    initialize: function (options) {
         _.extend({}, this.defaults, options);
     },
 
-    defaults: {
-        displayType: 'element',     // TODO: should this be changed to 'link'?
-        linkType:  'and',
-        postType: null,
-        absTime: -1,
-        evolving: false,
+    defaults: function () {
+        return {
+            displayType: 'element',     // TODO: should this be changed to 'link'?
+            linkType: 'and',
+            postType: null,
+            absTime: -1,
+            evolving: false,
+        }
     },
 
 });
@@ -44,31 +48,33 @@
  * Backbone Model of Constraints
  */
 var ConstraintBBM = Backbone.Model.extend({
-    initialize: function(options){
+    initialize: function (options) {
         _.extend({}, this.defaults, options)
     },
 
-    defaults: {
-        type: null,           // Options are '=', '<', and '<='
-        srcID: null,
-        destID: null,
-        srcRefTP: null,       // Reference Time Point A,B,C, etc.
-        destRefTP: null,      // Reference Time Point A,B,C, etc.
-        /** 
-         * Absolute time points are only used with the '=' type of operator.
-         *  If a timepoint is not given -1 should be assigned as the default value.
-         */   
-        absTP: -1,
+    defaults: function () {
+        return {
+            type: null,           // Options are '=', '<', and '<='
+            srcID: null,
+            destID: null,
+            srcRefTP: null,       // Reference Time Point A,B,C, etc.
+            destRefTP: null,      // Reference Time Point A,B,C, etc.
+            /** 
+             * Absolute time points are only used with the '=' type of operator.
+             *  If a timepoint is not given -1 should be assigned as the default value.
+             */
+            absTP: -1,
+        }
     },
 
     /**
      * Checks if type of constraint is null
      * If so, constraint has not been fully assigned
      */
-    isComplete: function(){
-        return (!(this.get('type') == null) && 
-                !(this.get('srcID') == null) &&
-                !(this.get('destID') == null));
+    isComplete: function () {
+        return (!(this.get('type') == null) &&
+            !(this.get('srcID') == null) &&
+            !(this.get('destID') == null));
     },
 
 });
