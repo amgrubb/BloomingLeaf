b657ba5ddc7b9384a0152b3d078fcde08955e010
A. M. Grubb <amgrubb@smith.edu>
2021-08-11 14:26:38 -0400

    Backend CSP Solver Update - Part 2 (#533)
    This PR implements the updated CSP calls for evolving functions, relationships, and constraints.
    See list of commits for full details.
    
    * Add getters for intention functions
    
    * update function to call backend
    
    * Added code to handle initial value read.
    
    * Added basic call for backend next state.
    
    * update depricated java
    
    - Updated depricated "new Integer()" with "Integer.valueOf()"
    - changed type of absTimeArr
    - improving model builder
    
    * Updated backend.
    
    * Refactoring links in backend.
    
    * Added DecompositionLink class.
    
    * Adding new creation of links.
    
    * Created new Intention item and cleaned up code.
    
    * Implementing Unique TP for intentions, links etc.
    
    * Restructured the code.
    
    * working on creating time points
    
    * Added constraints to the modelSpec.
    
    * Implemented creation of timepoints, variables, and ran solver.
    
    * Created draft of output file.
    
    * Got end-to-end in backend working.
    
    * Renamed `assignedEpoch` to `timePointAssignments`
    
    * Removed and supressed compilation warnings
    
    * Storing Current ClassPath
    
    * update backendStringifyCirc
    
    * Merged OMain with IPreviousAnalysis to be IOResult
    
    * Added TODOs.
    
    * Adding previous assignment infromation.
    
    * Added in previous values for assigned TP.
    
    * Adding next state analysis.
    
    * Deleted out of date classes.
    
    * Creating output for next state solution.
    
    * Updated Blooming.jar
    
    * Fixed bug with minimum value.
    
    * Refactored CSP Analysis, rewrote next state time point creation.
    
    * Added To Do Items.
    
    * Created list for next states.
    
    * Got initial next state file to output.
    
    * Got values out of next state.
    
    * Prepare model for initial trial.
    
    * Removing dead code.
    
    * Updating jar file.
    
    * Updated the front end to accept new variables.
    
    * deleted old line
    
    * Removed array of intentions from solver class.
    
    * Adding logic for forward contributions.
    
    * Added forward decomposition link constraints.
    
    * Fixed bug in link creation.
    
    * Working on backward links.
    
    * Implemented Backwards Analysis Constraints.
    
    * Moved static items into files.
    
    * Initial commit of CSPIntention - currently broken
    
    * Update CSPIntentions
    
    * small change
    
    * Finished initial pass of evolving functions.
    
    * Added user evaluations.
    
    * Added TODOs for next session.
    
    * Added initial functions for NB and TP Constraints
    
    * Created dead code, reused functions, and NBD-TP issue.
    
    * Adding new .jar file.
    
    * Added absTime for repeating function segments.
    
    * Removed unused items.
    
    * Add absTime for decompositions. Rename Main
    
    * Add UML diagrams RAW
    
    * Move DEBUG to main.
    
    * Update Blooming.jar

--- leaf-analysis/src/gson_classes/BIUserEvaluation.java
+++ leaf-analysis/src/gson_classes/BIUserEvaluation.java
@@ -2,7 +2,12 @@ package gson_classes;
 
 public class BIUserEvaluation {
     private Attributes attributes;
-
+	public String getAssignedEvidencePair() {
+		return attributes.assignedEvidencePair;
+	}
+	public Integer getAbsTime() {
+		return attributes.absTime;
+	}
     private class Attributes {
         String assignedEvidencePair;
         Integer absTime;
---
+++ leaf-analysis/src/simulation/Intention.java
@@ -0,0 +1,169 @@
+package simulation;
+
+import java.util.HashMap;
+
+import gson_classes.BIEvolvingFunction;
+import gson_classes.BIFunctionSegment;
+import gson_classes.BIUserEvaluation;
+import gson_classes.ICell;
+
+public class Intention extends AbstractLinkableElement {
+	private static int idcounter = 0;
+	
+	@SuppressWarnings("unused")
+	private Actor actor = null;
+	
+	//private IntentionalElementType type = IntentionalElementType.GOAL; 
+	
+	private HashMap<Integer, String> userEvals;
+	private FunctionSegment[] evolvingFunctions;
+	
+	public Intention(String uniqueID, String nodeName, Actor nodeActor, String nodeType,
+			BIEvolvingFunction nodeFunctions, HashMap<Integer, String> nodeEvals){
+		super(Intention.getNewBackendID(), nodeName, uniqueID);
+		this.actor = nodeActor;
+		//this.type = IntentionalElementType.getByBBMName(nodeType);
+		this.userEvals = nodeEvals;
+		this.evolvingFunctions = unrollBIEvolvingFunction(nodeFunctions);
+	}
+	
+	private static String getNewBackendID() {
+		String backID = String.format("%03d", idcounter);
+		idcounter++;
+		return backID;
+	}
+	
+	public String getRealFuncSegTP(String initialTP) {
+		char letter = initialTP.charAt(0);
+		int segNum = letter - 'A' + 1;
+		if (evolvingFunctions.length > (segNum + 1))
+			return null;
+		else
+			return evolvingFunctions[segNum].getStartTP();
+	}
+	public boolean updateRealFuncSegTP(String oldSegTP, String newSegTP) {
+		for (FunctionSegment seg : evolvingFunctions) 
+			if (seg.getStartTP().equals(oldSegTP)){
+				seg.updateStartTP(newSegTP);
+				return true;
+			}
+		return false;
+	}
+	
+	private FunctionSegment[] unrollBIEvolvingFunction(BIEvolvingFunction inFunc) {
+		if (!inFunc.getHasRepeat()) {
+			BIFunctionSegment[] biList = inFunc.getFunctionSegList();
+			FunctionSegment[] list = new FunctionSegment[biList.length];
+			for (int i = 0; i < list.length; i++) {
+				BIFunctionSegment seg = biList[i];
+				list[i] = new FunctionSegment(seg.getType(),seg.getRefEvidencePair(), 
+						seg.getStartTP(), seg.getStartAT(), this.id);
+			}
+			return list;
+		} else {	// Has repeat!
+			int repStartIndex = -1;
+			int repStopIndex = -1;
+			BIFunctionSegment[] biList = inFunc.getFunctionSegList();
+			for (int i = 0; i < biList.length; i++) {
+				if (biList[i].getStartTP().equals(inFunc.getRepStart()))
+						repStartIndex = i;
+				if (biList[i].getStartTP().equals(inFunc.getRepStop()))
+						repStopIndex = i;
+			}
+			if (repStartIndex == -1) 
+				repStartIndex = 0;
+			if (repStopIndex == -1)
+				repStopIndex = biList.length;
+			int repLength = repStopIndex - repStartIndex; 
+			int realStopIndex = repStartIndex + (repLength * inFunc.getRepCount());
+			int totalNumSegment = biList.length + ((inFunc.getRepCount() - 1) * repLength);
+
+			// Check to make sure if there is a repeating segment that it has a start time.
+			Integer repATLength = inFunc.getRepAbsTime();
+			Integer repAT = biList[repStartIndex].getStartAT();
+			if (repATLength != null && repAT == null)
+				throw new RuntimeException("Intention has repeating segment with absTime, but repStartAT is null.");
+
+			FunctionSegment[] list = new FunctionSegment[totalNumSegment];
+
+			int i = 0; // Corresponds to the list index for the new FunctionSegment[]
+			int s = 0; // Corresponds to the input segment index.
+			while(i < repStartIndex){
+				BIFunctionSegment seg = biList[s];
+				list[i] = new FunctionSegment(seg.getType(),seg.getRefEvidencePair(), 
+						seg.getStartTP(), seg.getStartAT(), this.id);
+				i++;
+				s++;
+			}
+			int rNum = 1;
+			while(i < realStopIndex){				
+				BIFunctionSegment seg = biList[s];
+				String repTP = "E" + this.id + ":R" + rNum + seg.getStartTP();
+				list[i] = new FunctionSegment(seg.getType(),seg.getRefEvidencePair(), 
+						repTP, repAT);
+				i++;
+				s++;
+				if (s == repStopIndex) {
+					rNum++;
+					s = repStartIndex;		
+				}
+				if (repATLength != null && repAT != null)
+					repAT += repATLength;
+			}
+			s = repStopIndex;
+			while(i < list.length) { 
+				BIFunctionSegment seg = biList[s];
+				list[i] = new FunctionSegment(seg.getType(),seg.getRefEvidencePair(), 
+						seg.getStartTP(), seg.getStartAT(), this.id);
+				i++;
+				s++;
+			}
+			return list;
+		}
+	}
+	
+	
+	public void assignNBFunction(String transitionTP, Integer transitionAT) {
+		this.evolvingFunctions = new FunctionSegment[2];
+		this.evolvingFunctions[0] = new FunctionSegment("C", "0000", "Initial", 0);
+		this.evolvingFunctions[1] = new FunctionSegment("R", "0000", transitionTP, transitionAT);
+	}
+	
+	/** Creates an intention to be added to the mode.
+	 *		Note: Assumes Actors have already been read into the model. 
+	 * @param iNode the input node to be added
+	 * @param modelSpec	the initial model specification containing actors
+	 * @return	the new intention
+	 */
+	public static Intention createIntention(ICell iNode, ModelSpec modelSpec) {
+		// Determine if intention has an actor.
+		Actor nodeActor = null;
+		if (iNode.getParent() != null) {
+			for(Actor actor : modelSpec.getActors()){
+				if(iNode.getParent().equals(actor.getUniqueID())){
+					nodeActor = actor;
+					break;
+				}
+			}
+		}
+		HashMap<Integer, String> userEvaluations = new HashMap<Integer, String>();
+		for (BIUserEvaluation eval : iNode.getIntention().getUserEvaluationList()) 
+			userEvaluations.put(eval.getAbsTime(), eval.getAssignedEvidencePair());
+		
+		Intention element = new Intention(iNode.getId(), iNode.getIntention().getNodeName(), 
+				nodeActor, iNode.getType(), iNode.getIntention().getEvolvingFunction(), 
+				userEvaluations);
+		
+		return element;
+	}
+
+	public FunctionSegment[] getEvolvingFunctions() {
+		return evolvingFunctions;
+	}
+
+	public HashMap<Integer, String> getUserEvals() {
+		return userEvals;
+	}
+	
+	
+}
