a7ae002124a0277eb74450e924872106656df7d5
i-m-foster <irenefoster64@gmail.com>
2021-08-11 09:49:36 -0400

    adding pages back to next state

--- leaf-ui/js/next-state.js
+++ leaf-ui/js/next-state.js
@@ -25,6 +25,7 @@
     window.onload = function(){
         analysis.page = jQuery.extend({}, window.opener.document);
         init();
+        renderNavigationSidebar();
     }
     
     function init(){
@@ -64,7 +65,165 @@
         //These object hold the request and results for the object.
         console.log("Request:" + JSON.stringify(myInputJSObject.request.toJSON()));
         console.log("Result:" + JSON.stringify(myInputJSObject.results.toJSON()));
+        console.log(myInputJSObject.request);
+        console.log(myInputJSObject.results);
         
+    }
+
+
+    function updateNodesValues(currentPage, step = 0){
+        if(currentPage == "")
+            currentPage = 0;
+    
+        //Set the currentState variable so it can be sent back to the original path
+        var i = 0;
+        for (let element of analysis.graph.getElements()) {
+            console.log(element);
+            // cell = analysis.elements[i];
+            // value = analysis.analysisResult.allSolution[currentPage].intentionElements[i].status[step];
+            // TODO: fix this line below
+            satValue = myInputJSObject.results.get('elementList')[i].status[myInputJSObject.results.get('elementList')[i].status.length - 1];
+            element.attr(".satvalue").value = satValue;
+    
+            //Change backend value to user friendly view
+            if ((satValue == "0001") || (satValue == "0011")) {
+                element.attr(".satvalue/text", "(F, ⊥)");
+                element.attr({text:{fill:'black'}});
+            } else if (satValue == "0010") {
+                element.attr(".satvalue/text", "(P, ⊥)");
+                element.attr({text:{fill:'black'}});
+            } else if ((satValue == "1000") || (satValue == "1100")){
+                element.attr(".satvalue/text", "(⊥, F)");
+                element.attr({text:{fill:'black'}});
+            } else if (satValue == "0100") {
+                element.attr(".satvalue/text", "(⊥, P)");
+                element.attr({text:{fill:'black'}});
+            } else if (satValue == "0110") {
+                element.attr(".satvalue/text", "(P, P)");
+                element.attr({text:{fill:'red'}});
+            } else if ((satValue == "1110") || (satValue == "1010")){
+                element.attr(".satvalue/text", "(P, F)");
+                element.attr({text:{fill:'red'}});
+            } else if ((satValue == "0111") || (satValue == "0101")){
+                element.attr(".satvalue/text", "(F, P)");
+                element.attr({text:{fill:'red'}});
+            } else if ((satValue == "1111") || (satValue == "1001") || (satValue == "1101") || (satValue == "1011") ){
+                element.attr(".satvalue/text", "(F, F)");
+                element.attr({text:{fill:'red'}});
+            } else if (satValue == "0000") {
+                element.attr(".satvalue/text", "(⊥,⊥)");
+                element.attr({text:{fill:'black'}});
+            } else {
+                element.removeAttr(".satvalue/d");
+            }
+            i++;
+        }
+    }    
+
+    function renderNavigationSidebar(currentPage = 0){
+        clear_pagination_values();
+    
+        var currentPageIn = document.getElementById("currentPage");
+        var num_states_lbl = document.getElementById("num_states_lbl");
+    
+        // TODO: have to update this, allSolutions is a hashmap
+        num_states_lbl.innerHTML += (myInputJSObject.results.get('allSolutions')["TNS-R"].length);
+    
+        currentPageIn.value = currentPage.toString();
+    
+        updatePagination(currentPage);
+        updateNodesValues(currentPage);
+    
+        // TODO: implement this back in eventually
+        // EVONextState.setColorBlindFromPrevWindow();
+        // EVONextState.refresh();
+    }
+
+    function updatePagination(currentPage){
+        var pagination = document.getElementById("pagination");
+        // TODO: have to update this, allSolutions is a hashmap
+        var nextSteps_array_size = myInputJSObject.results.get('allSolutions')["TNS-R"].length;
+        if (nextSteps_array_size > 6){
+            renderPreviousBtn(pagination, currentPage);
+            if (currentPage - 3 < 0){
+                for(var i = 0; i < 6; i++){
+                    render_pagination_values(currentPage, i);
+                }
+            } else {
+                if (currentPage + 3 < nextSteps_array_size){
+                    for (i = currentPage - 3; i < currentPage + 3; i++){
+                        render_pagination_values(currentPage, i);
+                    }
+                } else {
+                    for (i = currentPage - 3; i < nextSteps_array_size; i++){
+                        render_pagination_values(currentPage, i);
+                    }
+                }
+            }
+            renderForwardBtn(pagination, currentPage)
+        } else {
+            renderPreviousBtn(pagination, currentPage);
+            for (var i = 0; i < nextSteps_array_size; i++){
+                render_pagination_values(currentPage, i);
+            }
+            renderForwardBtn(pagination, currentPage)
+        }
+    }
+
+    function renderPreviousBtn(pagination, currentPage){
+        var value;
+        if (currentPage == 0){
+            value = 0;
+        } else {
+            value = currentPage - 1;
+        }
+        pagination.innerHTML += '<a href="#" onclick="renderNavigationSidebar('+value.toString()+')">&laquo;</a>';
+    }
+    
+    function renderForwardBtn(pagination, currentPage){
+        var value;
+        // TODO: have to update this, allSolutions is a hashmap
+        var nextSteps_array_size = myInputJSObject.results.get('allSolutions')["TNS-R"].length;
+    
+        if (currentPage == nextSteps_array_size-1){
+            value = currentPage;
+        } else {
+            value = currentPage + 1;
+        }
+        pagination.innerHTML += '<a href="#" onclick="renderNavigationSidebar(' + value.toString() + ')">&raquo;</a>';
+    }
+    
+    function render_pagination_values(currentPage, i){
+        var pagination = document.getElementById("pagination");
+        if (currentPage == i){
+            pagination.innerHTML += '<a href="#" class="active" onclick="renderNavigationSidebar(' + i.toString() + ')">' + i.toString() + '</a>';
+        } else {
+            pagination.innerHTML += '<a href="#" onclick="renderNavigationSidebar(' + i.toString() + ')">' + i.toString() + '</a>';
+        }
+    }
+
+    function clear_pagination_values(){
+        var pagination = document.getElementById("pagination");
+        var num_states_lbl = document.getElementById("num_states_lbl");
+        var currentPageIn = document.getElementById("currentPage");
+    
+        pagination.innerHTML = "";
+        num_states_lbl.innerHTML = "";
+        currentPageIn.value = "";
+    }
+
+    function goToState(){
+        var requiredState = parseInt(document.getElementById("requiredState").value);
+        // TODO: have to update this, allSolutions is a hashmap
+        var nextSteps_array_size = myInputJSObject.results.get('allSolutions')["TNS-R"].length;
+    
+        if ((requiredState != "NaN") && (requiredState > 0)){
+            if (requiredState > nextSteps_array_size){
+                renderNavigationSidebar(nextSteps_array_size);
+            } else {
+                renderNavigationSidebar(requiredState);
+            }
+        }
     }
 
 }
