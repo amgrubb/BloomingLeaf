4a9f599bc0673c8eced8dfdcfae0d12c248f3474
Yesugen <ybaatartogtokh@smith.edu>
2021-10-14 18:10:28 -0400

    Added analysis-only class to analysisID

--- leaf-ui/js/onFunctions.js
+++ leaf-ui/js/onFunctions.js
@@ -588,7 +588,6 @@ paper.on("link:options", function (cell) {
         configInspector = new ConfigInspector({ collection: configCollection });
         $('#configID').append(configInspector.el);
         configInspector.render();
-        $('#analysisID').css("display", "");
 
         // Remove model only elements 
         $('.model-only').css("display", "none");
@@ -644,7 +643,7 @@ paper.on("link:options", function (cell) {
             EVO.switchToModelingMode(selectResult);
             // Remove configInspector and analysis view
             configInspector.remove();
-            $('#analysisID').css("display", "none");
+
             // TODO: Determine if we should be setting action to null on all configs
             configCollection.findWhere({ selected: true }).set('action', null);
 
