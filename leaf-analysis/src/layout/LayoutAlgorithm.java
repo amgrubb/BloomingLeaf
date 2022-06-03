944dc32b1619d5d8c4f36f7b291af6b057f91a42
violetbi <xbi@smith.edu>
2022-06-03 15:05:18 -0400

    add calculation methods

--- leaf-analysis/src/layout/LayoutAlgorithm.java
+++ leaf-analysis/src/layout/LayoutAlgorithm.java
@@ -6,7 +6,7 @@ import static java.lang.Math.max;
 public class LayoutAlgorithm {
 	// models
 	ModelSpec model;
-    int maxIter;
+    int maxIter = 20;
 
 	/**
 	 * Initialize LayoutAlgorithm and run layoutModels
@@ -27,9 +27,80 @@ public class LayoutAlgorithm {
         // TODO: some logging object init with filename
 	}
 
-	public ModelSpec layoutModels(){
-        //list nodePositions = model.actorlistPositions + model.intentionlistPositions
+    /**
+     * Calculate the force attraciton given by
+     * the expression (d*d/Cn)
+     * @return the force attraction between two elements
+     */
+    private double[] getAttraction(Node<T> n1, Node<T> n2) {
+        if (n1 != n2) {
+            double distX = n1.getX() - n2.getX();
+            double distY = n1.getY() - n2.getY();
+            double dist = Math.sqrt(distX * distX + distY * distY);
+            double k; /// default coefficient, set a value to it
+            double coef = k * Math.sqrt(area/nodePositions.size()); // area?
+            double forceSum = dist * dist / coef;
+            //
+            // double cos = distX / dist;
+            // double sin = distY / dist;
+            // double forceX = cos * forceSum;
+            // double forceY = sin * forceSum;
+            // if(n2.getX()<n1.getX()) forceX = -forceX;
+            // if(n2.getY()<n1.getY()) forceY = -forceY;
+            // double[] forces = new double[2];
+            // forces[0] = forceX;
+            // forces[1] = forceY;
+        }
+        return forceSum;
+    }
 
+    /**
+     * Calculate the force repulsion given by
+     * the expression (Cn*Cn/d)
+     * @return the force repulsion between two elements
+     */
+    private double[] getRepulsion(Node<T> n1, Node<T> n2) {
+        if (n1 != n2) {
+            double distX = n1.getX() - n2.getX();
+            double distY = n1.getY() - n2.getY();
+            double dist = Math.sqrt(distX * distX + distY * distY);
+            double k; /// default coefficient, set a value to it
+            double coef = k * Math.sqrt(area/nodePositions.size()); // area?
+            double forceSum = coef * coef / dist;
+            //
+            // double cos = distX / dist;
+            // double sin = distY / dist;
+            // double forceX = cos * forceSum;
+            // double forceY = sin * forceSum;
+            // if(n2.getX()<n1.getX()) forceX = -forceX;
+            // if(n2.getY()<n1.getY()) forceY = -forceY;
+            // double[] forces = new double[2];
+            // forces[0] = forceX;
+            // forces[1] = forceY;
+        }
+        return forceSum;
+    }
+    
+    /*
+     * Calculate the angle between two nodes
+     * 
+     */
+    public double angleBetween(n1,n2) {
+        if(n1 != n2){
+            double distX = Math.abs(n1.getX() - n2.getX());
+            double distY = Math.abs(n1.getY() - n2.getY());
+            double theta = Math.atan(distY/distX);
+        }
+        return theta;
+    }
+
+    public ModelSpec layoutModels(){
+        //list nodePositions = model.actorlistPositions + model.intentionlistPositions
+        List<T> nodePositions = new ArrayList<T>(actorList);
+        nodePositions.addAll(intentionList); //what's the type of the list
+        for(int i = 0; i < maxIter; i ++){
+            
+        }
 		//for i < maxIter:
 
             //sum up forces for the X and Y directions
