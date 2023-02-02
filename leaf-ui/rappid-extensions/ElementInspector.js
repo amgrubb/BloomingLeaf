18feab31b24582ccda9833c8425585cf999c33b1
Venus Nguyen <89676581+Vbeany@users.noreply.github.com>
2023-02-02 14:53:57 -0500

    Sets it back to none
    I originally removed this line because whenever you set the func type to Constant the sat val is automatically set to be none but will continue to reset when called and so overrides the previous wanted sat val. But we still need it to be set the first time at least.

--- leaf-ui/rappid-extensions/ElementInspector.js
+++ leaf-ui/rappid-extensions/ElementInspector.js
@@ -1016,6 +1016,7 @@ var FuncSegView = Backbone.View.extend({
                 this.model.set('refEvidencePair', this.initSatValue);
             } else if (this.index != 0 && this.model.get('current')) {
                 this.$("#seg-sat-value").prop('disabled', '');
+                this.model.set('refEvidencePair', "0000");
                 this.$("#seg-sat-value").val(this.model.get('refEvidencePair'));
             } else {
                 this.$("#seg-sat-value").prop('disabled', true);
