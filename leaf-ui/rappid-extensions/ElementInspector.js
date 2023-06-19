80b3d51016a50b50dd755ca6c2ce42608aa23d31
Karenna Kung <95237168+kkung9@users.noreply.github.com>
2023-06-19 11:51:14 -0400

    After simulating, looks for chart before displaying chart

--- leaf-ui/rappid-extensions/ElementInspector.js
+++ leaf-ui/rappid-extensions/ElementInspector.js
@@ -730,14 +730,16 @@ var ElementInspector = Backbone.View.extend({
     } else if (this.intention.get('evolvingFunction') != null && this.intention.getUserEvaluationBBM(0).get('assignedEvidencePair') !== null && this.intention.get('evolvingFunction').get('type') === 'NT') {
         // Clear previous chart values
         this.chart.reset();
-        // Gets the chart canvas
-        var context = $("#chart").get(0).getContext("2d");
         // Adds the initial satisfaction as a single point to the chart
         this.chart.addDataSet(0, [satisfactionValuesDict[this.intention.getUserEvaluationBBM(0).get('assignedEvidencePair')].chartVal], false);
         // Renders the chart
-        this.chart.display(context);
+        if ($("#chart").get(0)) { // if the chart is displayed on the page
+            // Gets the chart canvas
+            var context = $("#chart").get(0).getContext("2d");
+            this.chart.display(context);
         }
-    },
+    }
+},
 
     /**
      * Called whenever the html is updated. Renders the views for the FunctionSegmentBBMs and adds an absTime label
