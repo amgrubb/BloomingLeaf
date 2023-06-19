e4b5173b89ed30b93fd97a5b6a9da15fa2954496
Karenna Kung <95237168+kkung9@users.noreply.github.com>
2023-06-19 11:41:30 -0400

    Only tries to redisplay chart if chart was already in window

--- leaf-ui/rappid-extensions/ElementInspector.js
+++ leaf-ui/rappid-extensions/ElementInspector.js
@@ -1134,8 +1134,6 @@ var FuncSegView = Backbone.View.extend({
             var funcType = this.intention.get('evolvingFunction').get('type');
             var initVal = satisfactionValuesDict[this.intention.getUserEvaluationBBM(0).get('assignedEvidencePair')].chartVal;
             this.chart.reset();
-            // Get the chart canvas
-            var context = $("#chart").get(0).getContext("2d");
 
             // Render preview for user defined function types
             if (funcType == "UD") {
@@ -1179,7 +1177,11 @@ var FuncSegView = Backbone.View.extend({
                     this.chart.addDataSet(0, [initVal], false);
                 }
             }
-            this.chart.display(context);
+            if ($("#chart").get(0)) { // if the chart is displayed on the page
+                // Get the chart canvas
+                var context = $("#chart").get(0).getContext("2d");
+                this.chart.display(context);
+            }
         }
     },
 
