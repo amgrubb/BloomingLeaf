033558e7fe726c42a41d74c3d03edbc7a9cb523c
kkung9 <kkung@smith.edu>
2025-09-10 14:50:31 -0400

    test tracking when looking at PDF

--- leaf-ui/js/initializeElements.js
+++ leaf-ui/js/initializeElements.js
@@ -178,3 +178,14 @@ dialog.on("action:tutorials", function() {
 		GuideBox.why.get(GuideBox.step[1]).showGuideBox();
 	}
 })
+
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        // User has switched away or minimized the browser
+        trackClick("NA", "left BloomingLeaf tab");
+        // You could send a beacon here too if needed
+    } else {
+        // User is back on the page
+        trackClick("NA", "rejoined BloomingLeaf tab");
+    }
+});
